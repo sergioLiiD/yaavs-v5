417cc7d23350735baaf513f9fd54177790a43db7
hoomsystems <sergio@hoom.mx>
2025-05-30 11:22:26 +0200

    cambio de color en botones

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,19 +5,19 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#FEBF19] focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "bg-gray-900 text-gray-50 hover:bg-gray-900/90",
+        default: "bg-[#FEBF19] text-gray-900 hover:bg-[#FEBF19]/90",
         destructive:
           "bg-red-500 text-gray-50 hover:bg-red-500/90",
         outline:
-          "border border-gray-200 bg-white hover:bg-gray-100 hover:text-gray-900",
+          "border border-[#FEBF19] bg-white hover:bg-[#FEBF19]/10 hover:text-[#FEBF19]",
         secondary:
           "bg-gray-100 text-gray-900 hover:bg-gray-100/80",
-        ghost: "hover:bg-gray-100 hover:text-gray-900",
-        link: "text-gray-900 underline-offset-4 hover:underline",
+        ghost: "hover:bg-[#FEBF19]/10 hover:text-[#FEBF19]",
+        link: "text-[#FEBF19] underline-offset-4 hover:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
