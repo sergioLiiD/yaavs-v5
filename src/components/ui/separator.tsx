da53797429da69c6eba0e523d25f76c9f67c7451
hoomsystems <sergio@hoom.mx>
2025-08-20 13:50:57 +0200

    Fix: Simplificar componente Separator sin dependencias externas

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -1,24 +1,18 @@
 "use client"
 
 import * as React from "react"
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
-
 import { cn } from "@/lib/utils"
 
-const Separator = React.forwardRef<
-  React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(
-  (
-    { className, orientation = "horizontal", decorative = true, ...props },
-    ref
-  ) => (
-    <SeparatorPrimitive.Root
+interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
+  orientation?: "horizontal" | "vertical"
+}
+
+const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(
+  ({ className, orientation = "horizontal", ...props }, ref) => (
+    <div
       ref={ref}
-      decorative={decorative}
-      orientation={orientation}
       className={cn(
-        "shrink-0 bg-border",
+        "shrink-0 bg-gray-200",
         orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
         className
       )}
@@ -26,6 +20,6 @@ const Separator = React.forwardRef<
     />
   )
 )
-Separator.displayName = SeparatorPrimitive.Root.displayName
+Separator.displayName = "Separator"
 
 export { Separator }
